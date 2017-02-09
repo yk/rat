a255a4eeddf74b437a7fedcaaf1a5a3049f40075
Yannic Kilcher <yannic.kilcher@gmail.com>
2017-02-09 09:47:06 +0100

    printing

--- rat/worker.py
+++ rat/worker.py
@@ -165,8 +165,8 @@ def run_config(rat_config, experiment, config):
 
         all_files = utils.get_all_files(path)
         new_fns = [fn for fn in all_files if os.path.getmtime(fn) >= start_time]
-        print(all_files)
-        print(new_fns)
+        print([os.path.getmtime(fn) for fn in all_files])
+        print(start_time)
         # new_fids = utils.save_file_tree(grid, path, new_fns, exclude_patterns=['latest'])
         new_fids = utils.save_file_tree(grid, path, new_fns, exclude_patterns=[])
 
